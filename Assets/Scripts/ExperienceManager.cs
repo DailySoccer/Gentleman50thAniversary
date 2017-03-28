7705ceba092df4f5829c0e4795329d64c7df048c
korgan00 <korgan00@hotmail.com>
2017-03-28 19:37:15 +0200

    fixes and out of the richard branson scene

--- Assets/Scripts/ExperienceManager.cs
+++ Assets/Scripts/ExperienceManager.cs
@@ -3,20 +3,34 @@
 public class ExperienceManager : MonoBehaviour {
 
 	public Camera mainCamera;
+	public GameObject mainCameraContainer;
 
 	private Animator generalAnimator;
 	public GameObject skySphere;
 	public GameObject world;
 
-	public Animator richardBransonCover;
-	public Animator richardBransonBlackBox;
-	public GameObject bransonCooperScene;
+
 
 	void Start() {
 		generalAnimator = GetComponent<Animator>();
 		generalAnimator.SetTrigger("Start");
 	}
 
+
+	public void HideMainCamera() {
+		mainCamera.gameObject.SetActive(false);
+	}
+
+	public void ShowMainCamera() {
+		mainCamera.gameObject.SetActive(true);
+	}
+
+	#region Richard Branson
+
+	public Animator richardBransonCover;
+	public Animator richardBransonBlackBox;
+	public GameObject richardBransonWorldElements;
+	public GameObject bransonCooperScene;
 
 	public void ActivateRichardBransonCover() {
 		richardBransonCover.SetTrigger("ActivateCover");
@@ -31,20 +45,87 @@ public class ExperienceManager : MonoBehaviour {
 	}
 
 	public void OpenRichardBransonBlackBox() {
+		richardBransonCover.SetTrigger("HideCover");
+	}
+
+	public void ActivateRichardBransonScene() {
+		richardBransonWorldElements.SetActive(false);
+		skySphere.SetActive(false);
+		bransonCooperScene.SetActive(true);
+	}
+
+	#endregion
+
+
+	#region Cooper
+
+	public Animator cooperBlackBox;
+	public GameObject cooperBlackBoxContainer;
+	public GameObject cooperWorldElements;
+
+	public void ActivateCooperBlackBox() {
+		cooperBlackBoxContainer.SetActive(true);
+		federerBlackBoxContainer.SetActive(true);
+
+		cooperBlackBox.SetTrigger("ActivateBox");
+
+		mainCamera.gameObject.SetActive(true);
+	}
+
+	/*
+	public void ActivateRichardBransonCover() {
+		richardBransonCover.SetTrigger("ActivateCover");
+	}
+
+
+	public void DeactivateRichardBransonBlackBox() {
+		richardBransonBlackBox.SetTrigger("DeactivateBox");
+	}
+
+	public void OpenRichardBransonBlackBox() {
 		richardBransonBlackBox.SetTrigger("OpenBox");
 		richardBransonCover.SetTrigger("HideCover");
 	}
 
 	public void ActivateRichardBransonScene() {
-		world.SetActive(false);
+		richardBransonWorldElements.SetActive(false);
 		skySphere.SetActive(false);
 		bransonCooperScene.SetActive(true);
 	}
+*/
+	#endregion
 
-	public void HideMainCamera() {
-		mainCamera.gameObject.SetActive(false);
+
+	#region Federer
+
+	public Animator federerBlackBox;
+	public GameObject federerBlackBoxContainer;
+
+	public void ActivateFedererBlackBox() {
+		federerBlackBox.SetTrigger("ActivateBox");
+	}
+
+	/*
+	public void ActivateRichardBransonCover() {
+		richardBransonCover.SetTrigger("ActivateCover");
 	}
 
 
+	public void DeactivateRichardBransonBlackBox() {
+		richardBransonBlackBox.SetTrigger("DeactivateBox");
+	}
+
+	public void OpenRichardBransonBlackBox() {
+		richardBransonBlackBox.SetTrigger("OpenBox");
+		richardBransonCover.SetTrigger("HideCover");
+	}
+
+	public void ActivateRichardBransonScene() {
+		richardBransonWorldElements.SetActive(false);
+		skySphere.SetActive(false);
+		bransonCooperScene.SetActive(true);
+	}
+*/
+	#endregion
 
 }
