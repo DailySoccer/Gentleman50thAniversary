635727ac5528a30950e7e5439d9c4dd6598235b6
korgan00 <korgan00@hotmail.com>
2017-04-03 19:52:41 +0200

    loading modal and more

--- Assets/Scripts/TitlesSceneManager.cs
+++ Assets/Scripts/TitlesSceneManager.cs
@@ -5,16 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class TitlesSceneManager : MonoBehaviour {
 
+	public GameObject loadingObj;
+	public List<GameObject> hideObjects;
+
 
 	public void GoGallery() {
+		Loading();
 		SceneManager.LoadScene ("Gallery", LoadSceneMode.Single);
 	}
 
 	public void Play() {
+		Loading();
 		SceneManager.LoadScene ("GentlemanPresentation", LoadSceneMode.Single);
 	}
 
 	public void GoToTitleScreen() {
 		SceneManager.LoadScene("TitleScreenReplay", LoadSceneMode.Single);
+	}
+
+	private void Loading() {
+		//cam.transform.rotation = Quaternion.identity;
+		foreach(GameObject g in hideObjects) {
+			g.SetActive(false);
+		}
+		loadingObj.SetActive(true);
 	}
 }
