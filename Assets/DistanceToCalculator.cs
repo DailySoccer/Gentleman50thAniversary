4b9fb2cb3b68544ec741255a9f155418b7786357
korgan00 <korgan00@hotmail.com>
2017-04-03 11:19:05 +0200

    .

---
+++ Assets/DistanceToCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceToCalculator : MonoBehaviour {
+
+
+	public GameObject objTo;
+
+	void Update() {
+		if (objTo != null) {
+			Debug.Log(Vector3.Distance(this.transform.position, objTo.transform.position));
+		}
+	}
+}
